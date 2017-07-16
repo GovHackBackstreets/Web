41568d2976517077a7efb2c3c9c6b7844013c4f1
Thomas Allen <tom@telega.org>
2017-07-16 11:04:57 +0100

    added tree visualisation, updated api calls

--- js/main.js
+++ js/main.js
@@ -12,6 +12,8 @@ $("#showChain").click(function(){
 		$('.p2').fadeIn();
 	});
 
+	buildChart();
+
 })
 
 $("#backPage").click(function(){
@@ -26,7 +28,7 @@ getFoodItem = function( itemId ){
 	requestSettings = {
   "async": true,
   "crossDomain": true,
-  "url": "http://govhacksapi.herokuapp.com/stuff/" + itemId ,
+  "url": "http://govhacksapi.herokuapp.com/scan/" + itemId ,
   "method": "GET",
 }
 
@@ -38,39 +40,167 @@ $.ajax(requestSettings).done(function (response) {
 
 
 updatePage = function(r){
-
-	 $('#foodName').text(r.foodName);
-	 $('#foodCategory').text(r.foodCategory);
-	 $('#locationName').text(r.steps[0].locationName);
-	 $('#facilityName').text(r.steps[0].facilityName);
+	console.log(r);
+	 $('#foodName').text(r.name);
+	 $('#locationName').text(r.stamps[0].locationName);
+	 $('#facilityName').text(r.stamps[0].facilityName);
 	 $('.map').html(
-	 	'<iframe width="240" height="240" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q='+r.steps[0].geoloc.lat+','+r.steps[0].geoloc.long+'&hl=es;z=14&amp;output=embed"></iframe>'
+	 	'<iframe width="240" height="240" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q='+r.stamps[0].geoloc.lat+','+r.stamps[0].geoloc.long+'&hl=es;z=14&amp;output=embed"></iframe>'
 	 )
 
-	 for(var i = 0; i < r.steps.length; i++){
+	 for(var i = 0; i < r.stamps.length; i++){
 
-	 	var locationName = r.steps[i].locationName;
-	 	var lat = r.steps[i].geoloc.lat;
-	 	var lon = r.steps[i].geoloc.lon;
-    	var dateIn = r.steps[i].dateIn;
-    	var dateOut = r.steps[i].dateOut
-    	var facilityName = r.steps[i].facilityName;
-        var facilityAccredation = r.steps[i].facilityAccredation;
-        var conditionIn = r.steps[i].conditionIn;
-        var conditionOut = r.steps[i].conditionOut; 
-        var facilityChemicals = r.steps[i].facilityChemicals;
-        var facilityAllergens = r.steps[i].facilityAllergens;
+	 	var locationName = r.stamps[i].locationName;
+	 	var lat = r.stamps[i].geoloc.lat;
+	 	var lon = r.stamps[i].geoloc.lon;
+    	
+    	var facilityName = r.stamps[i].facilityName;
+        
+        var chemicalContaminents = r.stamps[i].chemicalContaminents;
+        var microbialSafety = r.stamps[i].microbialSafety; 
+        
+        var physicalQuality = r.stamps[i].physicalQuality;
+        var temperatureControl= r.stamps[i].temperatureControl;
+        
         var stepLabel = 1+i;
 
         $('.steps').append('<hr><div class = row><div class = "col-md-4"><h4>'+stepLabel+'</h4><h5>'
-         + locationName +'</h5><h5>' + facilityName + '</h5><h5>'+ facilityAccredation+ 
-         '</h5></div><div class = "col-md-4"><h4>Allergens</h4><h5>' + facilityAllergens + 
-         '</h5><h4>Chemicals</h4><h5>'+ facilityChemicals + '</h5></div><div class = "col-md-4"><h4>Date In: ' + 
-         dateIn + '</h4><h4>Date Out: ' + dateOut + '</h4><h4>Condition In:<br>' + conditionIn +
-         '</h4><h4>Condition Out:<br>' + conditionOut + '</h4></div>');
+         + locationName +'</h5><h5>' + facilityName + '</h5></h5></div><div class = "col-md-4"><h4>Microbial Safety</h4><h5>' + microbialSafety + 
+         '</h5><h4>Chemicals</h4><h5>'+ chemicalContaminents + '</h5></div><div class = "col-md-4"><h4>Physical Quality </h4><h5>' + physicalQuality + '</h5><h4>Temperature Control</h4><h5>' + temperatureControl +
+         '</h4></div>');
 
 	 }
 
 	 $('.results').fadeIn();
+
+}
+
+function buildChart(){
+
+
+var data =[];
+
+var indexOfId = function(arr, id, index) {
+    if (!index) { index = 0; }
+    if (arr[index].itemId == id) {
+      return index;
+    }
+    return ((index += 1) >= arr.length) ? -1 : indexOfId(arr, id, index);
+};
+
+getFoodItem = function( itemId ){
+  requestSettings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "http://govhacksapi.herokuapp.com/scan/all/" + itemId ,
+  "method": "GET",
+}
+
+$.ajax(requestSettings).done(function (response) {
+  var foodItems = response;
+
+
+
+
+  function getChildren(index){
+
+      var foodNode = {
+          "name":foodItems[index].name,
+          "children": []
+        }
+        var children = [];
+
+      if (foodItems[index].links.length > 0){
+      
+      for(var i = 0; i < foodItems[index].links.length; i++){
+        var idx = (indexOfId(foodItems,foodItems[index].links[i]));
+
+        children.push({
+          "name": foodItems[idx].name,
+          "children": getChildren(idx)
+        });
+      }
+      
+      foodNode.children=children;
+      
+  }
+  return children;
+
+  }
+
+   var foodNode = {
+          "name":foodItems[0].name,
+          "children": getChildren(0)
+        }
+
+
+  data.push( foodNode);
+
+  
+  root = data[0];
+  
+  drawGraph(root);
+});
+}
+
+getFoodItem( $('#foodItemId').val() );
+
+
+var margin = {top: 0, right: 120, bottom: 0, left: 120},
+ width = 960 - margin.right - margin.left,
+ height = 1060 - margin.top - margin.bottom;
+ 
+
+var svg = d3.select("svg").attr("width", width + margin.right + margin.left)
+ .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+ .attr("transform", "translate(" + margin.left + "," + margin.top + ")"); 
+    
+var i = 0;
+
+var tree = d3.layout.tree()
+ .size([height, width]);
+
+var diagonal = d3.svg.diagonal()
+ .projection(function(d) { return [d.y, d.x]; });
+
+
+
+function drawGraph(source) {
+
+  var nodes = tree.nodes(root).reverse(),
+   links = tree.links(nodes);
+
+  nodes.forEach(function(d) { d.y = d.depth * 180; });
+
+  var node = svg.selectAll("g.node")
+   .data(nodes, function(d) { return d.id || (d.id = ++i); });
+
+  var nodeEnter = node.enter().append("g")
+   .attr("class", "node")
+   .attr("transform", function(d) { 
+    return "translate(" + d.y + "," + d.x + ")"; });
+
+  nodeEnter.append("circle")
+   .attr("r", 8)
+   .style("fill", "#fff");
+
+  nodeEnter.append("text")
+   .attr("x", function(d) { 
+    return d.children || d._children ? -13 : 13; })
+   .attr("dy", "10px")
+   .attr("text-anchor", function(d) { 
+    return d.children || d._children ? "end" : "start"; })
+   .text(function(d) { return d.name; })
+   .style("fill-opacity", 1);
+
+  var link = svg.selectAll("path.link")
+   .data(links, function(d) { return d.target.id; });
+
+  link.enter().insert("path", "g")
+   .attr("class", "link")
+   .attr("d", diagonal);
+
+}
 
 }
